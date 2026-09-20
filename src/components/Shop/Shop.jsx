9651bcf85a7c686b059dbfff7da3c841cad9319b
Srabonti <somayaaktarshabonti721@gmail.com>
2026-09-20 09:40:29 +0600

    add shop page loader data and math.ceil and pagination to 1-8

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -3,17 +3,16 @@ import { addToDb, deleteShoppingCart, getShoppingCart } from '../../utilities/fa
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css';
-import { Link } from 'react-router-dom';
+import { Link, useLoaderData } from 'react-router-dom';
 
 const Shop = () => {
     const [products, setProducts] = useState([]);
     const [cart, setCart] = useState([])
+const {count} = useLoaderData();
+const itemsPerPage = 10;
+const itemOfNumber = Math.ceil(count/itemsPerPage);
 
-    useEffect(() => {
-        fetch('http://localhost:5000/products')
-            .then(res => res.json())
-            .then(data => setProducts(data))
-    }, []);
+const pages = [...Array(itemOfNumber).keys()]
 
     useEffect(() => {
         const storedCart = getShoppingCart();
@@ -81,6 +80,11 @@ const Shop = () => {
                         <button className='btn-proceed'>Review Order</button>
                     </Link>
                 </Cart>
+            </div>
+            <div className="pagination">
+                {
+                    pages.map(page => <button key={page}>{page + 1}</button>)
+                }
             </div>
         </div>
     );
